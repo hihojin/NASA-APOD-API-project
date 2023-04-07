a3d79c338aebede148165ee4c8abb37d8f5c7037
Hyojin Kwak <hyojinkwak@Hyojins-MBP.lan>
2023-04-07 13:04:55 -0700

    404 vercel

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http:localhost:8000',
+      '/api': {
+        target: 'http://localhost:8000/',
+        changeOrigin: true,
+      }
     },
   },
   plugins: [react()],
